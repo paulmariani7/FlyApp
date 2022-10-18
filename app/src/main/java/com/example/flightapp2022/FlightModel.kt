853708a7c004107c6e07f8ef7c60e534affae0d3
sergio <miranda@goodbarber.com>
2022-10-18 13:29:45 +0200

    project start

---
+++ app/src/main/java/com/example/flightapp2022/FlightModel.kt
@@ -0,0 +1,18 @@
+package com.example.flightapp2022
+
+/**
+ * Created by sergio on 07/11/2021
+ * All rights reserved GoodBarber
+ */
+data class FlightModel (val icao24: String,
+                        val firstSeen: Long,
+                        val estDepartureAirport: String,
+                        val lastSeen: Long,
+                        val estArrivalAirport: String,
+                        val callsign: String,
+                        val estDepartureAirportHorizDistance: Int,
+                        val estDepartureAirportVertDistance: Int,
+                        val estArrivalAirportHorizDistance: Int,
+                        val estArrivalAirportVertDistance: Int,
+                        val departureAirportCandidatesCount: Int,
+                        val arrivalAirportCandidatesCount: Int)
---
+++ app/src/main/java/com/example/flightapp2022/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.flightapp2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Utils.generateAirportList()
+    }
+}
